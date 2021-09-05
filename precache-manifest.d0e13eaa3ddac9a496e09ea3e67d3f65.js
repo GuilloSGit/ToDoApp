5825ef85a9872e292eb9216f92c20d7fb5637c89
Guillermo D Andrada <guillermoandrada@gmail.com>
2021-09-05 16:50:11 -0300

    Add files via upload

---
+++ precache-manifest.d0e13eaa3ddac9a496e09ea3e67d3f65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7c1c03a937a2f22206fef8c8670d87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6f728d4635d424804d0",
+    "url": "/static/css/main.43712ffc.chunk.css"
+  },
+  {
+    "revision": "995502519ee109a8b2c5",
+    "url": "/static/js/2.4d732ae9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.4d732ae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6f728d4635d424804d0",
+    "url": "/static/js/main.f17719aa.chunk.js"
+  },
+  {
+    "revision": "8eaaebe7465f69e4c1db",
+    "url": "/static/js/runtime-main.4f71040b.js"
+  },
+  {
+    "revision": "a1aefeb408f743d7b51217e264e7e3d1",
+    "url": "/static/media/bgPic.a1aefeb4.jpg"
+  }
+]);
